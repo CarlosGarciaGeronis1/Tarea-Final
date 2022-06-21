5b626846deb67828d8ca7b18a47597bf169ea4df
CarlosGarciaG <66147914+Hiler150620@users.noreply.github.com>
2022-06-20 22:06:29 -0500

    Update server.c

--- server.c
+++ server.c
@@ -19,22 +19,23 @@ void serve(int s)
     char buffer[MSGSIZE];
     int size, i = 0;
     struct stat buf;
-    const char espacio[2] = " ";
-    const char punto[2] = ".";
+    const char space[2] = " ";
     char *token;
-    char *token2;
     char filetosend[100];
     char fileType[100];
     FILE *sin = fdopen(s, "r");
     FILE *sout = fdopen(s, "w");
     int status;
+
+    // Reads the request from the client
     while (fgets(buffer, MSGSIZE, sin) != NULL)
     {
         printf("%d - [%s]\n", ++i, buffer);
+        // A blank line is found -> end of headers
         if (i == 1)
         {
             int indice = 1;
-            token = strtok(buffer, espacio);
+            token = strtok(buffer, space);
             while (token != NULL)
             {
                 if (indice == 2)
@@ -42,7 +43,7 @@ void serve(int s)
                     if (strcmp(strcpy(filetosend, token), "/") == 0)
                     {
                         strcpy(filetosend, "listoffiles.txt");
-                        strcpy(fileType, ".txt");
+                        strcpy(fileType,".txt");
                         char *filename = "listoffiles.txt";
                         FILE *fp = fopen(filename, "w");
                         if (fp == NULL)
@@ -54,6 +55,7 @@ void serve(int s)
                         DIR *dr = opendir(".");
                         if (dr == NULL)
                         {
+                            printf("Error opening the file %s", filename);
                             printf("Could not open current directory");
                             return 0;
                         }
@@ -67,32 +69,41 @@ void serve(int s)
                     else
                     {
                         strcpy(filetosend, token + 1);
+                        strcpy(fileType,token+1);
                     }
                 }
                 indice++;
-                token = strtok(NULL, espacio);
+                token = strtok(NULL, space);
             }
+            //==========================================
+
+            //==========================================
         }
         if (buffer[0] == '\r' && buffer[1] == '\n')
         {
             break;
         }
     }
+
+    // Builds response
     sprintf(buffer, "HTTP/1.0 200 OK\r\n");
     fputs(buffer, sout);
+
     sprintf(buffer, "Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
     fputs(buffer, sout);
+    char *comparacion;
+    strcpy(comparacion,fileType);
     char *contiene;
-    contiene = strstr(filetosend, ".html");
+    contiene = strstr(comparacion, ".html");
     if (contiene)
     {
         sprintf("Content-Type:", "text/html\r\n");
         fputs(buffer, sout);
-        printf("EL TIPO DE ARCHIVO SE MANDO COMO HTML");
+       
         //strcpy(fileType, "Content-Type: text/html");
     }
     char *contiene2;
-    contiene2 = strstr(filetosend, ".jpg");
+    contiene2 = strstr(comparacion, ".jpg");
     if (contiene2)
     {
         sprintf("Content-Type:", "image/jpg\r\n");
@@ -100,7 +111,7 @@ void serve(int s)
         //strcpy(fileType, "Content-Type: image/jpg");
     }
     char *contiene3;
-    contiene3 = strstr(filetosend, ".png");
+    contiene3 = strstr(comparacion, ".png");
     if (contiene3)
     {
         sprintf("Content-Type:", "image/png\r\n");
@@ -108,7 +119,7 @@ void serve(int s)
         //strcpy(fileType, "Content-Type: image/png");
     }
     char *contiene4;
-    contiene4 = strstr(filetosend, ".txt");
+    contiene4 = strstr(comparacion, ".txt");
     if (contiene4)
     {
         sprintf("Content-Type:", "text/plain\r\n");
@@ -139,21 +150,27 @@ int main()
     int sd, sdo, size, r;
     struct sockaddr_in sin, pin;
     socklen_t addrlen;
+
+    // 1. Crear el socket
     sd = socket(AF_INET, SOCK_STREAM, 0);
 
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(PORT);
+
+    // 2. Asociar el socket a IP:port
     r = bind(sd, (struct sockaddr *)&sin, sizeof(sin));
     if (r < 0)
     {
         perror("bind");
         return -1;
     }
+    // 3. Establecer backlog
     listen(sd, 5);
 
     addrlen = sizeof(pin);
+    // 4. Esperar conexion
     while ((sdo = accept(sd, (struct sockaddr *)&pin, &addrlen)) > 0)
     {
         if (!fork())
